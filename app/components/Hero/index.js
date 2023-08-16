27055b4fc930fec241f3ca2306934a3a76801e00
niiwade <niilantewade77@gmail.com>
2023-08-16 13:02:23 +0000

    fixed gallery

--- app/components/Hero/index.js
+++ app/components/Hero/index.js
@@ -60,7 +60,7 @@ const Hero = () => {
         <div data-image-overlay className={styles.hero__imageOverlay}></div>
         <Image
           data-image
-          src="/images/am5.jpeg"
+          src="/images/am.jpg"
           layout="fill" // This makes the image take up the whole parent container (hero__image)
           objectFit="cover"
           alt="Blob"
